23f1d3a625acc53dc5fa5f475cd09f6e4201141f
Ugik DEV <ugik.dev@gmail.com>
2024-09-13 17:02:34 +0700

    config excel maatwebsite

--- config/app.php
+++ config/app.php
@@ -173,6 +173,7 @@ return [
         Yajra\DataTables\DataTablesServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
         // Intervention\Image\ImageServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -191,6 +192,7 @@ return [
         // 'Example' => App\Facades\Example::class,
         'Helper' => App\Helpers\Helpers::class,
         // 'Image' => Intervention\Image\Facades\Image::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ])->toArray(),
 
 ];
